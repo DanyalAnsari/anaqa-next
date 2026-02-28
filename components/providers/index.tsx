22a4537cba453262d88e838a59e285d4500f4b75
DanyalAnsari <mh.danyalansari@gmail.com>
2026-02-28 23:48:14 +0530

    feat: Implement theme support using next-themes and integrate into the root layout, adding zustand dependency.

---
+++ components/providers/index.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { ReactNode } from "react";
+
+import { ThemeProvider } from "@/components/providers/theme-provider";
+
+export function Providers({ children }: { children: ReactNode }) {
+	return (
+		<ThemeProvider
+			attribute="class"
+			defaultTheme="system"
+			enableSystem
+			disableTransitionOnChange
+		>
+			{children}
+		</ThemeProvider>
+	);
+}
