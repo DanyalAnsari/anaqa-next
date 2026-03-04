551fb0bb807f4163687b5e44dbb82960f238d993
DanyalAnsari <mh.danyalansari@gmail.com>
2026-03-04 06:18:32 +0530

    feat(auth): enable email verification and add verification email handler

--- lib/auth.ts
+++ lib/auth.ts
@@ -3,6 +3,7 @@ import { nextCookies } from "better-auth/next-js";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@/database";
 import schema from "@/database/schemas";
+import { sendEmail } from "./nodemailer";
 
 export const auth = betterAuth({
 	database: drizzleAdapter(db, {
@@ -11,6 +12,16 @@ export const auth = betterAuth({
 	}),
 	emailAndPassword: {
 		enabled: true,
+		requireEmailVerification: true,
+	},
+	emailVerification: {
+		sendVerificationEmail: async ({ user, url, token }, request) => {
+			void sendEmail({
+				to: user.email,
+				subject: "Verify your email address",
+				html: `<h1>Click the link to verify your email: <a href="${url}">${url}</a></h1>`,
+			});
+		},
 	},
 	plugins: [nextCookies()],
 });
